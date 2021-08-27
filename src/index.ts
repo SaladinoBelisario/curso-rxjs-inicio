e23822550a9c6161d575802b9b2c0fc75d9fdf27
Saladino Belisario <SaladinoBelisario@protonmail.ch>
2021-08-26 20:34:30 -0500

    Subscribing and Unsubscribing

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
+import {Observable, Observer} from 'rxjs';
 
+const observer: Observer<any> = {
+    next    : value => console.log('next:', value),
+    error   : error => console.log('error:', error),
+    complete: () => console.log('complete')
+};
 
-console.log('Hola Mundo!');
+const intervalo$ = new Observable<number>( subscriber =>{
+    let count = 0;
+    const interval = setInterval(() => {
+        count++;
+        subscriber.next(count);
+    }, 3000);
 
+    return () =>{
+        clearInterval(interval);
+        console.log('Intervalo destruido');
+    }
+});
 
+const subscription = intervalo$.subscribe( num => console.log('Num:', num));
 
-
-
-
-
+setTimeout(() => {
+    subscription.unsubscribe();
+}, 3000);
